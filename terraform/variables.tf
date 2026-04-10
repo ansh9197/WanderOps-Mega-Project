d51b4232e5066efbf32c09a83428a9081b4aabba
Harshit Arora <ansh55028@gmail.com>
2026-04-10 11:26:34 +0530

    Update variables.tf
    Updated the default value description for instance_type variable to provide guidance on choosing instance types.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,5 +10,5 @@ variable "ami_id" {
 
 variable "instance_type" {
   description = "Instance type for the EC2 instance"
-  default     = "t2.large"
+  default     = ("choose according to the need for ex- t3.small, t2.large etc")
 }
